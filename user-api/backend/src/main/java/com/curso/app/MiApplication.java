370059d8075dcd408536c61bcccd4bc8c245483c
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-02-23 11:02:59 +0100

    cucumber

--- user-api/backend/src/main/java/com/curso/app/MiApplication.java
+++ user-api/backend/src/main/java/com/curso/app/MiApplication.java
@@ -1,7 +1,10 @@
-package app.curso.app;
+package com.curso.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("com.curso")
 @EnableJpaRepositories(basePackages={"com.curso"})
